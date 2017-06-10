9f110b328a078c5a90498225d034e3d0e3a73c16
budokoromo <budokoromo.gmail.com>
2017-06-10 15:40:37 +0200

    after crash before mrcrunch

---
+++ automation.bckup/jsr223/example.py
@@ -0,0 +1,13 @@
+scriptExtension.importPreset("RuleSupport")
+scriptExtension.importPreset("RuleSimple")
+
+
+class myRule(SimpleRule):
+    def execute(self, module, inputs):
+        print "Hello World from Jython"
+
+sRule = myRule()
+sRule.setTriggers([Trigger("aTimerTrigger", "timer.GenericCronTrigger", Configuration({"cronExpression": "0/15 * * * * ?"}))])
+
+automationManager.addRule(sRule)
+
